e129c288a20706e7641263edf13e8cbff6abf8c1
yonatan <fnatanoy@gmail.com>
2020-01-27 16:36:50 +0200

    update setup

--- setup.py
+++ setup.py
@@ -20,5 +20,15 @@ setup(
     description='',
     long_description=open('README.md').read(),
     keywords=['report','analysis','figures','plot'],
-    download_url='https://github.com/fnatanoy/reportity/archive/v_021.tar.gz',
+    download_url='https://github.com/fnatanoy/reportity/archive/v_022.tar.gz',
+    classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
