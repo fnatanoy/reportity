8b49a750a9ad15f249c926b87b1b50442fa5cea9
kenshoo <fnatanoy@gmail.com>
2021-01-04 20:01:11 +0200

    update ver

--- setup.py
+++ setup.py
@@ -11,18 +11,17 @@ setup(
         'mpld3',
         'numpy',
         'pandas',
-        # 'Jinja2 == 2.10.1',
     ],
     dependency_links=[
         'git+git://github.com/mpld3/mpld3@master#egg=mpld3',
     ],
-    version='1.0',
+    version='1.2',
     license='MIT',
     description='this is the best package for reports',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     keywords=['report','analysis','figures','plot'],
-    download_url='https://github.com/fnatanoy/reportity/archive/ver1.1.tar.gz',
+    download_url='https://github.com/fnatanoy/reportity/archive/ver1.2.tar.gz',
     classifiers=[
         'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',      # Define that your audience are developers
