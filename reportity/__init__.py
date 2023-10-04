53e4f92fa84b087c56d2a5c15b93b4df87967fcf
Geraud d'Aspremont <geraud.daspremont@kenshoo.com>
2023-10-04 13:15:32 +0200

    Fix if/elif when not using plotly to render graph (#4)
    * Small fix
    * Swith to pyproject
    * bump version 1.21.0 -> 1.22.0
    * Update bumpver setting
    * bump version 1.22.0 -> 1.23.0
    * Remove build folder
    * Change bumv not to push

--- reportity/__init__.py
+++ reportity/__init__.py
@@ -1,1 +1,2 @@
 __all__ = ['reportity']
+__version__ = "1.23.0"
--- setup.py
+++ setup.py
@@ -1,35 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='reportity',
-    url='https://github.com/fnatanoy/reportity',
-    author='Yonatan Faigenbaum',
-    author_email='fnatanoy@gmail.com',
-    packages=['reportity'],
-    install_requires=[
-        'matplotlib',
-        'mpld3',
-        'numpy',
-        'pandas',
-    ],
-    dependency_links=[
-        'git+git://github.com/mpld3/mpld3@master#egg=mpld3',
-    ],
-    version='1.21',
-    license='MIT',
-    description='this is the best package for reports',
-    long_description=open('README.md').read(),
-    long_description_content_type="text/markdown",
-    keywords=['report','analysis','figures','plot'],
-    download_url='https://github.com/fnatanoy/reportity/archive/ver1.21.tar.gz',
-    classifiers=[
-        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Developers',      # Define that your audience are developers
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',   # Again, pick a license
-        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-)
+setup()
